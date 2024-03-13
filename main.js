571c546041de61698360b917c2999e0b56faa3be
Mashal Afzal <mashal.afzal2996@gmail.com>
2024-03-13 08:07:34 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+var famous_person = "Robin Sharma";
+var message = '${famous_person} Once said, "One day our bodies grow old and our posessions fade All that has valued become the impact we have left on others."';
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,3 @@
+let famous_person: string= "Robin Sharma"
+let message: string = '${famous_person} Once said, "One day our bodies grow old and our posessions fade All that has valued become the impact we have left on others."';
+console.log(message);
